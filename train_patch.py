fea4fdf6e5f06be4ce439d8ddf9f8b12ccc51264
Hao-yu liu <89632727+catfishQAQ@users.noreply.github.com>
2025-04-16 01:33:24 -0700

    Update train_patch.py
    Signed-off-by: Hao-yu liu <89632727+catfishQAQ@users.noreply.github.com>

--- train_patch.py
+++ train_patch.py
@@ -81,48 +81,49 @@ class PatchTrainer:
         if cfg.augment_image:
             transforms = T.Compose(
                 [
-                    T.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 1)),
-                    T.ColorJitter(brightness=0.2, hue=0.04, contrast=0.1),
-                    T.RandomAdjustSharpness(sharpness_factor=2),
+                    T.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 1)),#加入高斯模糊，模拟图像模糊的情况
+                    T.ColorJitter(brightness=0.2, hue=0.04, contrast=0.1),#颜色扰动，包括亮度、色调、对比度的微调
+                    T.RandomAdjustSharpness(sharpness_factor=2),#随机调整图像的清晰度（锐化效果）
                 ]
             )
 
         # load training dataset
         self.train_loader = torch.utils.data.DataLoader(
             YOLODataset(
-                image_dir=cfg.image_dir,
-                label_dir=cfg.label_dir,
-                max_labels=cfg.max_labels,
-                model_in_sz=cfg.model_in_sz,
-                use_even_odd_images=cfg.use_even_odd_images,
-                transform=transforms,
-                filter_class_ids=cfg.objective_class_id,
-                min_pixel_area=cfg.min_pixel_area,
-                shuffle=True,
+                image_dir=cfg.image_dir,#图像所在目录
+                label_dir=cfg.label_dir,#标签目录，YOLO 格式的 .txt 文件，每张图一个标签文件
+                max_labels=cfg.max_labels,#	每张图最多读取多少个标签
+                model_in_sz=cfg.model_in_sz,#模型输入图像大小（H, W）
+                use_even_odd_images=cfg.use_even_odd_images,#是否只用奇数/偶数编号图片
+                transform=transforms,#图像增强 transform
+                filter_class_ids=cfg.objective_class_id,#只保留这些目标类别的框
+                min_pixel_area=cfg.min_pixel_area,#忽略小于该面积的目标框，单位像素
+                shuffle=True,#是否打乱图像顺序
             ),
             batch_size=cfg.batch_size,
             shuffle=True,
             num_workers=4,
-            pin_memory=True if self.dev.type == "cuda" else False,
+            pin_memory=True if self.dev.type == "cuda" else False,#如果使用 GPU，将数据固定在内存加快传输
         )
-        self.epoch_length = len(self.train_loader)
-
+        self.epoch_length = len(self.train_loader)#统计一个 epoch 中包含多少个 batch
+    #启动 TensorBoard（一个可视化工具）并创建日志写入器的函数
     def init_tensorboard(self, log_dir: str = None, port: int = 6006, run_tb=True):
         """Initialize tensorboard with optional name."""
         if run_tb:
             while is_port_in_use(port) and port < 65535:
                 port += 1
                 print(f"Port {port - 1} is currently in use. Switching to {port} for tensorboard logging")
-
+            #创建一个 TensorBoard 实例；配置它的参数（读取目录 + 端口）；
             tboard = program.TensorBoard()
             tboard.configure(argv=[None, "--logdir", log_dir, "--port", str(port)])
             url = tboard.launch()
             print(f"Tensorboard logger started on {url}")
 
         if log_dir:
+            #返回 SummaryWriter 日志写入器
             return SummaryWriter(log_dir)
         return SummaryWriter()
-
+    #根据指定的类型（灰色 or 随机），生成一个对抗 patch，作为训练优化的起点
     def generate_patch(self, patch_type: str, pil_img_mode: str = "RGB") -> torch.Tensor:
         """
         Generate a random patch as a starting point for optimization.
@@ -131,14 +132,17 @@ class PatchTrainer:
             patch_type: Can be 'gray' or 'random'. Whether or not generate a gray or a random patch.
             pil_img_mode: Pillow image modes i.e. RGB, L https://pillow.readthedocs.io/en/latest/handbook/concepts.html#modes
         """
+        #设置 patch 的通道数
         p_c = 1 if pil_img_mode in {"L"} else 3
         p_w, p_h = self.cfg.patch_size
+        #如果类型是 "gray"，生成一个像素值为 0.5 的常量补丁
         if patch_type == "gray":
             adv_patch_cpu = torch.full((p_c, p_h, p_w), 0.5)
+        #如果类型是 "random"，生成一个像素值是随机的 patch
         elif patch_type == "random":
             adv_patch_cpu = torch.rand((p_c, p_h, p_w))
         return adv_patch_cpu
-
+    #从指定路径读取一张图片，并将其转换为一个 PyTorch 张量格式的 patch 图像，用于作为对抗补丁的初始值
     def read_image(self, path, pil_img_mode: str = "RGB") -> torch.Tensor:
         """
         Read an input image to be used as a patch.
@@ -146,8 +150,10 @@ class PatchTrainer:
         Arguments:
             path: Path to the image to be read.
         """
+        #用 PIL.Image 打开图像文件，.convert(pil_img_mode) 把图像转换成指定的颜色通道格式
         patch_img = Image.open(path).convert(pil_img_mode)
         patch_img = T.Resize(self.cfg.patch_size)(patch_img)
+        #把 PIL 图像转换成 PyTorch 的 Tensor 格式，数值范围会变成 [0.0, 1.0]，ToTensor() 会自动把 (H, W, C) 格式转成 (C, H, W) height width channel
         adv_patch_cpu = T.ToTensor()(patch_img)
         return adv_patch_cpu
 
@@ -165,6 +171,7 @@ class PatchTrainer:
             json.dump(self.cfg, json_f, ensure_ascii=False, indent=4)
 
         # fix loss targets
+        #配置损失函数目标
         loss_target = self.cfg.loss_target
         if loss_target == "obj":
             self.cfg.loss_target = lambda obj, cls: obj
@@ -185,13 +192,15 @@ class PatchTrainer:
             adv_patch_cpu = self.generate_patch("random", self.cfg.patch_img_mode)
         else:
             adv_patch_cpu = self.read_image(self.cfg.patch_src, self.cfg.patch_img_mode)
+        #设为可训练
         adv_patch_cpu.requires_grad = True
-
+        #使用 Adam 优化器，如果 loss 在 50 个 epoch 内没有下降，就降低学习率
         optimizer = optim.Adam([adv_patch_cpu], lr=self.cfg.start_lr, amsgrad=True)
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min", patience=50)
 
         start_time = time.time()
         for epoch in range(1, self.cfg.n_epochs + 1):
+            #epoch 初始化设置，out_patch_path: 当前轮次保存的 patch 路径，ep_loss: 当前 epoch 总 loss；min_tv_loss: TV loss 的下限；zero_tensor: 用于不启用某项 loss 时作为替代
             out_patch_path = osp.join(patch_dir, f"e_{epoch}.png")
             ep_loss = 0
             min_tv_loss = torch.tensor(self.cfg.min_tv_loss, device=self.dev)
@@ -204,6 +213,7 @@ class PatchTrainer:
                     img_batch = img_batch.to(self.dev, non_blocking=True)
                     lab_batch = lab_batch.to(self.dev, non_blocking=True)
                     adv_patch = adv_patch_cpu.to(self.dev, non_blocking=True)
+                    #开始训练
                     adv_batch_t = self.patch_transformer(
                         adv_patch,
                         lab_batch,
@@ -214,6 +224,7 @@ class PatchTrainer:
                         rand_loc=self.cfg.random_patch_loc,
                     )
                     p_img_batch = self.patch_applier(img_batch, adv_batch_t)
+                    # resize 到模型输入大小
                     p_img_batch = F.interpolate(p_img_batch, (self.cfg.model_in_sz[0], self.cfg.model_in_sz[1]))
 
                     if self.cfg.debug_mode:
@@ -226,27 +237,29 @@ class PatchTrainer:
                         img.save(osp.join(self.cfg.log_dir, "train_patch_applied_imgs", f"b_{i_batch}.jpg"))
 
                     with autocast() if self.cfg.use_amp else nullcontext():
+                        #模型预测 + 损失计算
                         output = self.model(p_img_batch)[0]
                         max_prob = self.prob_extractor(output)
+                        #计算三个额外的 regularization loss
                         sal = self.sal_loss(adv_patch) if self.cfg.sal_mult != 0 else zero_tensor
                         nps = self.nps_loss(adv_patch) if self.cfg.nps_mult != 0 else zero_tensor
                         tv = self.tv_loss(adv_patch) if self.cfg.tv_mult != 0 else zero_tensor
-
+                    #总 loss + 反向传播 + 更新
                     det_loss = torch.mean(max_prob)
-                    sal_loss = sal * self.cfg.sal_mult
+                    sal_loss = sal * self.cfg.sal_mult/权重
                     nps_loss = nps * self.cfg.nps_mult
                     tv_loss = torch.max(tv * self.cfg.tv_mult, min_tv_loss)
 
                     loss = det_loss + sal_loss + nps_loss + tv_loss
                     ep_loss += loss
-
+                    #梯度下降优化补丁参数。
                     loss.backward()
                     optimizer.step()
                     optimizer.zero_grad(set_to_none=True)
                     # keep patch in cfg image pixel range
                     pl, ph = self.cfg.patch_pixel_range
                     adv_patch_cpu.data.clamp_(pl / 255, ph / 255)
-
+                    #定期记录当前 patch 和各类 loss 到 TensorBoard
                     if i_batch % self.cfg.tensorboard_batch_log_interval == 0:
                         iteration = self.epoch_length * epoch + i_batch
                         self.writer.add_scalar("total_loss", loss.detach().cpu().numpy(), iteration)
@@ -260,10 +273,12 @@ class PatchTrainer:
                     if i_batch + 1 < len(self.train_loader):
                         del adv_batch_t, output, max_prob, det_loss, p_img_batch, sal_loss, nps_loss, tv_loss, loss
                         # torch.cuda.empty_cache()  # note emptying cache adds too much overhead
+            #每个 epoch 结束，平均 loss 并更新学习率调度器
             ep_loss = ep_loss / len(self.train_loader)
             scheduler.step(ep_loss)
 
             # save patch after every patch_save_epoch_freq epochs
+            #每隔一定轮数（epoch），把当前优化得到的对抗补丁保存为图片文件，保存在指定的目录下
             if epoch % self.cfg.patch_save_epoch_freq == 0:
                 img = T.ToPILImage(self.cfg.patch_img_mode)(adv_patch_cpu)
                 img.save(out_patch_path)
@@ -271,6 +286,7 @@ class PatchTrainer:
                 # torch.cuda.empty_cache()  # note emptying cache adds too much overhead
 
             # run validation to calc asr on val set if self.val_dir is not None
+            #每隔固定轮数（val_epoch_freq），在验证集上评估当前的 adversarial patch 攻击效果
             if all([self.cfg.val_image_dir, self.cfg.val_epoch_freq]) and epoch % self.cfg.val_epoch_freq == 0:
                 with torch.no_grad():
                     self.val(epoch, out_patch_path)
@@ -283,10 +299,10 @@ class PatchTrainer:
         patch_img = T.Resize(self.cfg.patch_size)(patch_img)
         adv_patch_cpu = T.ToTensor()(patch_img)
         adv_patch = adv_patch_cpu.to(self.dev)
-
+        #加载验证图片的路径，使用 IMG_EXTNS 限定图片后缀名
         img_paths = glob.glob(osp.join(self.cfg.val_image_dir, "*"))
         img_paths = sorted([p for p in img_paths if osp.splitext(p)[-1] in IMG_EXTNS])
-
+        #备份 patch 缩放比例，并设置为验证用值
         train_t_size_frac = self.patch_transformer.t_size_frac
         self.patch_transformer.t_size_frac = [0.3, 0.3]  # use a frac of 0.3 for validation
         # to calc confusion matrixes and attack success rates later
@@ -294,24 +310,28 @@ class PatchTrainer:
         all_patch_preds = []
 
         m_h, m_w = self.cfg.model_in_sz
-        cls_id = self.cfg.objective_class_id
-        zeros_tensor = torch.zeros([1, 5]).to(self.dev)
+        cls_id = self.cfg.objective_class_id#从配置文件中读取你要攻击或评估的“目标类别 ID”，并赋值给变量 cls_id
+        zeros_tensor = torch.zeros([1, 5]).to(self.dev)#没检测到
         #### iterate through all images ####
         for imgfile in tqdm(img_paths, desc=f"Running val epoch {epoch}"):
             img_name = osp.splitext(imgfile)[0].split("/")[-1]
             img = Image.open(imgfile).convert("RGB")
+            #使用 pad_to_square() 补成正方形，再 resize 到模型输入大小
             padded_img = pad_to_square(img)
             padded_img = T.Resize(self.cfg.model_in_sz)(padded_img)
 
             #######################################
             # generate labels to use later for patched image
             padded_img_tensor = T.ToTensor()(padded_img).unsqueeze(0).to(self.dev)
+            #用干净图预测目标框，通过 NMS 去除重叠框
             pred = self.model(padded_img_tensor)
             boxes = non_max_suppression(pred, conf_thresh, nms_thresh)[0]
             # if doing targeted class performance check, ignore non target classes
             if cls_id is not None:
                 boxes = boxes[boxes[:, -1] == cls_id]
+            #保存预测得到的“干净标签框”
             all_labels.append(boxes.clone())
+            #格式转换成 YOLO 样式的 [class, cx, cy, w, h]
             boxes = xyxy2xywh(boxes)
 
             labels = []
@@ -327,6 +347,7 @@ class PatchTrainer:
                 padded_img_drawn.save(osp.join(self.cfg.log_dir, "val_clean_imgs", img_name + ".jpg"))
 
             # use a filler zeros array for no dets
+            #处理无目标情况
             label = np.asarray(labels) if labels else np.zeros([1, 5])
             label = torch.from_numpy(label).float()
             if label.dim() == 1:
@@ -336,7 +357,7 @@ class PatchTrainer:
             # Apply proper patches
             img_fake_batch = padded_img_tensor
             lab_fake_batch = label.unsqueeze(0).to(self.dev)
-
+            #判断是否有目标，有则贴 patch
             if len(lab_fake_batch[0]) == 1 and torch.equal(lab_fake_batch[0], zeros_tensor):
                 # no det, use images without patches
                 p_tensor_batch = padded_img_tensor
@@ -352,7 +373,7 @@ class PatchTrainer:
                     rand_loc=self.cfg.random_patch_loc,
                 )
                 p_tensor_batch = self.patch_applier(img_fake_batch, adv_batch_t)
-
+            #对贴了 patch 的图再次预测目标框
             pred = self.model(p_tensor_batch)
             boxes = non_max_suppression(pred, conf_thresh, nms_thresh)[0]
             # if doing targeted class performance check, ignore non target classes
@@ -367,11 +388,12 @@ class PatchTrainer:
                     all_patch_preds[-1], p_img_pil, self.cfg.class_list
                 )
                 p_img_pil_drawn.save(osp.join(self.cfg.log_dir, "val_patch_applied_imgs", img_name + ".jpg"))
-
+        #统计所有预测和目标框
         # reorder labels to (Array[M, 5]), class, x1, y1, x2, y2
         all_labels = torch.cat(all_labels)[:, [5, 0, 1, 2, 3]]
         # patch and noise labels are of shapes (Array[N, 6]), x1, y1, x2, y2, conf, class
         all_patch_preds = torch.cat(all_patch_preds)
+        #计算 ASR（攻击成功率）
         asr_s, asr_m, asr_l, asr_a = PatchTester.calc_asr(
             all_labels, all_patch_preds, class_list=self.cfg.class_list, cls_id=cls_id
         )
